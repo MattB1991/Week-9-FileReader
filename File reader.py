3553462b5f62d23c435b96002638244c95b11281
MattB1991 <110066706+MattB1991@users.noreply.github.com>
2022-08-03 19:14:08 -0500

    Update File reader.py
    I am trying to get the program to ask the right questions at the right time. For now anyway.

--- File reader.py
+++ File reader.py
@@ -19,3 +19,26 @@ wrote to the file system and display the file contents to the user for
 validation purposes. 
 Submit a link to your Github repository.
 '''
+import os
+
+filePath = input('What directory do you want to search?')
+fileName = input('What file am I looking for')
+completePath = filePath+fileName
+
+try:
+    if os.path.isfile(fileName):
+        print('File Exists')
+    if os.path.isdir(filePath): 
+        print('Directory Exists')
+    if os.path.exists(completePath): 
+        print('Complete path exists')
+        print('Complete Path',completePath)
+    with open(completePath, 'w') as fileHandle: 
+        fileHandle.write(input('What is your name?')
+        fileHandle.write(input('What is your address?')
+        fileHandle.write(input(' What is your phone number?')
+    with open(completePath, 'r') as fileHandle: 
+        data = fileHandle.read() 
+        print(data)
+except FileNotFoundError:
+    
